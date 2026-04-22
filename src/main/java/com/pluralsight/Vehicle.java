979f3dda9baddd1124e9b23a0764c747536d3780
mili Arsema <meleymm88@gmail.com>
2026-04-22 08:31:32 -0500

    Set vehicle constractor

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -8,4 +8,15 @@ public class Vehicle {
     int odometerReading;
     double price;
 
+    //constractor
+    void vehicle(long vehicleID, String makeModel, String color, int odometerReading, double price){
+        this.vehicleID = vehicleID;
+        this.makeModel = makeModel;
+        this.color = color;
+        this.odometerReading = odometerReading;
+        this.price = price;
+
+
+    }
+
 }
