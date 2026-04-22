18bd5da2c0ff8b38e9aacba7918b3a2b9b7cc91b
mili Arsema <meleymm88@gmail.com>
2026-04-22 08:28:57 -0500

    Set vehicle attributes

--- src/main/java/com/pluralsight/main.java
+++ src/main/java/com/pluralsight/main.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class main {
+    //user input system
+
+    //object: vehicleInventory
+
+
+    //body: array
+
+    //action: loop + counter + command (user input)
 }
