9f87c6c91679de6de3ffa67d337d84f0fb1eee2c
mili Arsema <meleymm88@gmail.com>
2026-04-22 08:32:33 -0500

    Set vehicle attribute setters and getters

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -16,7 +16,50 @@ public class Vehicle {
         this.odometerReading = odometerReading;
         this.price = price;
 
+    }
 
+
+    //getters: need return type
+    long getVehicleID(){
+        return this.vehicleID;
+    }
+
+    String getMakeModel(){
+        return this.makeModel;
+    }
+
+    String getColor(){
+        return this.color;
+    }
+
+    int getOdometerReading(){
+        return this.odometerReading;
+    }
+
+    double getPrice(){
+        return this.price;
+    }
+
+
+    //setters: Return=void + need parameters
+    void setVehicleID(long vehicleID){
+        this.vehicleID = vehicleID;
+    }
+
+    void setMakeModel(String makeModel){
+        this.makeModel = makeModel;
+    }
+
+    void setColor(String color){
+        this.color = color;
+    }
+
+    void setOdometerReading(int odometerReading){
+        this.odometerReading = odometerReading;
+    }
+
+    void setPrice(double price){
+        this.price = price;
     }
 
 }
